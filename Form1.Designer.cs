9b9b4fbf6cad637e90a3da05845b817360aa9081
Bahattin <72936563+BahattinCinar@users.noreply.github.com>
2023-06-12 02:34:47 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Final_Odev
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(134, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(157, 95);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(134, 153);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.ScrollAlwaysVisible = true;
+            this.listBox2.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.listBox2.Size = new System.Drawing.Size(157, 95);
+            this.listBox2.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Kişi ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 56);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(88, 38);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "Kişi sil";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 100);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 38);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Listeyi Sıfırla";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 153);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 38);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Çek";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 210);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 38);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Kazananlari goster";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 263);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Collections;
+
+namespace Final_Odev
+{
+    public partial class Form1 : Form
+    {
+        ArrayList aL = new ArrayList();
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string exit, usernames;
+            
+            try
+            {
+                exit = Interaction.InputBox("Cikmak icin hangi butonu kullanmak istiyorsunuz", "Cikis butonu secme ekrani", "x", 125, 125);
+
+                for (int i = 0; ; )
+                {
+                    usernames = Interaction.InputBox("Cekilise katilicak insanlarin isimlerini giriniz ", "Cekilis giris ekrani", "", 125, 125);
+                    
+                    if (usernames == exit)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        i++;
+                        aL.Add(usernames);
+                        listBox1.Items.Add($"{i}.Katilimci :{usernames}");
+                    }
+                }
+
+            }
+            catch (OverflowException error1)
+            {
+                MessageBox.Show($"Lutfen limitler dahilinde giriniz. {error1}");
+            }
+            catch (FormatException error2)
+            {
+                MessageBox.Show($"Lutfen giris turunuzu dogru giriniz. {error2}");
+            }
+            catch (Exception error3)
+            {
+                MessageBox.Show($"Bilinmeyen hatayla karsilastiniz. {error3}");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox2.Visible = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int winnum, wincount, a = 1;
+                Random rnd = new Random();
+
+                wincount = int.Parse(Interaction.InputBox("Cekilisi kac kisi kazanicak ?", "Cekilis kazanan sayisi", "3", 125, 125));
+
+                string[] winners = new string[wincount];
+
+                for (int i = 0; i <= wincount - 1; i++)
+                {
+                    winnum = rnd.Next(0, aL.Count);
+                    winners[i] = aL[winnum].ToString();
+                    aL.RemoveAt(winnum);
+                    listBox2.Items.Add(a + ". Kazanan : " + aL[i]);
+                    a++;
+                }
+            }
+            catch(OverflowException error1)
+            {
+                MessageBox.Show($"Lutfen limitler dahilinde giriniz. {error1}");
+            }
+            catch(FormatException error2)
+            {
+                MessageBox.Show($"Lutfen giris turunuzu dogru giriniz. {error2}");
+            }
+            catch (Exception error3)
+            {
+                MessageBox.Show($"Bilinmeyen hatayla karsilastiniz. {error3}");
+            }
+            finally
+            {
+                MessageBox.Show("Cekilisiniz Sona Ermistir.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox2.Visible = true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string selected = listBox1.SelectedItem.ToString();
+                listBox1.Items.Remove(listBox1.SelectedItem);
+                aL.Remove(selected);
+            }
+            catch (OverflowException error1)
+            {
+                MessageBox.Show($"Lutfen limitler dahilinde giriniz. {error1}");
+            }
+            catch (FormatException error2)
+            {
+                MessageBox.Show($"Lutfen giris turunuzu dogru giriniz. {error2}");
+            }
+            catch (Exception error3)
+            {
+                MessageBox.Show($"Bilinmeyen hatayla karsilastiniz. {error3}");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            aL.Clear();
+            listBox2.Visible = false;
+        }
+    }
+}
